9b81f16af2d8f49a67c8a84e307831ba98663b23
Julio Chaves <chaves.julio@gmail.com>
2023-09-24 02:52:29 +0100

    Moving logging config to test file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,49 +1,5 @@
 import asyncio
-from logging.config import dictConfig
 import pytest
-
-logger_format = "%(asctime)s:%(threadName)s:%(name)s:%(message)s"
-dictConfig(
-    {
-        "version": 1,
-        "formatters": {
-            "default": {
-                "format": logger_format,
-                "datefmt": "%Y-%m-%dT%H:%M:%S %z",
-            },
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "formatter": "default",
-                "stream": "ext://sys.stdout",
-            },
-            "fileHandler": {
-                "class": "logging.handlers.RotatingFileHandler",
-                "formatter": "default",
-                "filename": "log_monitor_test.log",
-                "level": "DEBUG",
-                "maxBytes": 1024000,
-                "backupCount": 3,
-            },
-        },
-        "loggers": {
-            "root": {
-                "handlers": ["console", "fileHandler"],
-                "level": "DEBUG",
-            },
-            "helpers.cw_log_consumer": {
-                "level": "DEBUG",
-            },
-            "helpers.cw_log_monitor": {
-                "level": "DEBUG",
-            },
-            "helpers.cw_log_producer": {
-                "level": "DEBUG",
-            },
-        },
-    }
-)
 
 
 @pytest.fixture
--- tests/test_cw_log_monitor.py
+++ tests/test_cw_log_monitor.py
@@ -3,7 +3,51 @@ import boto3
 from helpers.cw_log_monitor import LogMonitor
 from helpers.cw_log_producer import LogProducer
 import logging
+from logging.config import dictConfig
 import pytest
+
+logger_format = "%(asctime)s:%(threadName)s:%(name)s:%(message)s"
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": logger_format,
+                "datefmt": "%Y-%m-%dT%H:%M:%S %z",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "default",
+                "stream": "ext://sys.stdout",
+            },
+            "fileHandler": {
+                "class": "logging.handlers.RotatingFileHandler",
+                "formatter": "default",
+                "filename": "log_monitor_test.log",
+                "level": "DEBUG",
+                "maxBytes": 1024000,
+                "backupCount": 3,
+            },
+        },
+        "loggers": {
+            "root": {
+                "handlers": ["console", "fileHandler"],
+                "level": "DEBUG",
+            },
+            "helpers.cw_log_consumer": {
+                "level": "DEBUG",
+            },
+            "helpers.cw_log_monitor": {
+                "level": "DEBUG",
+            },
+            "helpers.cw_log_producer": {
+                "level": "DEBUG",
+            },
+        },
+    }
+)
 
 logger = logging.getLogger(__name__)
 
